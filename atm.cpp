4701dfadb51bba716f40852854f1a63fd587b3db
[Anup  Magar] <am6956275@gmail.com>
2025-03-23 22:12:12 +0545

    Add ATM class with constructor for account management

--- atm.cpp
+++ atm.cpp
@@ -5,3 +5,19 @@ int main() {
     cout << "Welcome to the ATM System!\n";
     return 0;
 }
+class ATM {
+    private:
+        string accountNumber;
+        string pin;
+        string accountHolder;
+        double balance;
+    
+    public:
+        ATM(string accNum, string accHolder, string userPin, double initialBalance) {
+            accountNumber = accNum;
+            accountHolder = accHolder;
+            pin = userPin;
+            balance = initialBalance;
+        }
+    };
+    
